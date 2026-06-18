5b94df970ef408004ef277939a70e7288c14115a
Nikolay Boshnakov <n_boshnakov@abv.bg>
2026-06-18 23:58:44 +0300

    Added Word2Vec and LogReg script

---
+++ 04_run_h04_word2vec_logreg.py
@@ -0,0 +1,23 @@
+from src.parse_data import load_and_split_data
+from src.modeling import train_evaluate_pipeline
+from src.utils import print_evaluation_metrics
+from src.features import SpacyVectorizer
+from sklearn.linear_model import LogisticRegression
+
+def main() -> None:
+
+    train_path = "liar_dataset/train.tsv"
+    test_path = "liar_dataset/test.tsv"
+
+    x_train, y_train = load_and_split_data(train_path)
+    x_test, y_test = load_and_split_data(test_path)
+
+    vectorizer = SpacyVectorizer(model_name="en_core_web_lg")
+    classifier = LogisticRegression(max_iter=1000)
+
+    model, test_accuracy, test_macro_f1 = train_evaluate_pipeline(x_train, y_train, x_test, y_test, vectorizer, classifier)
+
+    print_evaluation_metrics("H04 - Word2Vec (spaCy) + Logistic Regression", test_accuracy, test_macro_f1)
+
+if __name__ == '__main__':
+    main()

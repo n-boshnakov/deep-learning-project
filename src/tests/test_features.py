bc7c79ceaeb5a91b66c1008819b7e1ad6e6eece8
Nikolay Boshnakov <n_boshnakov@abv.bg>
2026-06-19 00:03:47 +0300

    Updated test_features with GenSim test

--- src/tests/test_features.py
+++ src/tests/test_features.py
@@ -2,9 +2,9 @@ import unittest
 import numpy as np
 from src import features
 
-class TestSpacyVectorizer(unittest.TestCase):
+class TestVectorizers(unittest.TestCase):
 
-    def test_when_text_is_passed_then_returns_matrix(self):
+    def test_when_text_is_passed_to_spacy_then_returns_matrix(self):
         # Arrange
         dummy_txt = ["Fake text", "this is some more text", "Another statement from a political leader."]
         vectorizer = features.SpacyVectorizer(model_name="en_core_web_md")
@@ -19,5 +19,20 @@ class TestSpacyVectorizer(unittest.TestCase):
         self.assertIsInstance(actual_matrix, np.ndarray)
         self.assertEqual(expected_matrix_rows, actual_matrix.shape[0])
         self.assertEqual(expected_matrix_columns, actual_matrix.shape[1])
-        
+
+    def test_when_text_is_passed_to_gensim_then_returns_matrix(self):
+        # Arrange
+        dummy_txt = ["Fake text", "this is some more text", "Another statement from a political leader."]
+        vectorizer = features.GensimVectorizer(model_name="glove-twitter-25")
+        expected_matrix_rows = 3
+        # 25 is the standard for the glove-twitter-25 GenSim model
+        expected_matrix_columns = 25
+
+        # Act
+        actual_matrix = vectorizer.fit_transform(dummy_txt)
+
+        # Assert
+        self.assertIsInstance(actual_matrix, np.ndarray)
+        self.assertEqual(expected_matrix_rows, actual_matrix.shape[0])
+        self.assertEqual(expected_matrix_columns, actual_matrix.shape[1])
 

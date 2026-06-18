6f800e19b02289fb3b92c22ec9085a1bb3afc07e
Nikolay Boshnakov <n_boshnakov@abv.bg>
2026-06-18 23:58:04 +0300

    Added test for features adapter

---
+++ src/tests/test_features.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+from src import features
+
+class TestSpacyVectorizer(unittest.TestCase):
+
+    def test_when_text_is_passed_then_returns_matrix(self):
+        # Arrange
+        dummy_txt = ["Fake text", "this is some more text", "Another statement from a political leader."]
+        vectorizer = features.SpacyVectorizer(model_name="en_core_web_md")
+        expected_matrix_rows = 3
+        # 30 is the standard for the en_core_web_md spaCy model
+        expected_matrix_columns = 300
+
+        # Act
+        actual_matrix = vectorizer.fit_transform(dummy_txt)
+
+        # Assert
+        self.assertIsInstance(actual_matrix, np.ndarray)
+        self.assertEqual(expected_matrix_rows, actual_matrix.shape[0])
+        self.assertEqual(expected_matrix_columns, actual_matrix.shape[1])
+        
+

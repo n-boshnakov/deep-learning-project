d0b0e709fc9387ab8d50e88b340230511b12e81f
Nikolay Boshnakov <n_boshnakov@abv.bg>
2026-06-19 17:31:23 +0300

    Added GloVe + LogReg/RandomForest script

---
+++ 05_run_h05_glove_logreg.py
@@ -0,0 +1,25 @@
+from src.parse_data import load_and_split_data
+from src.modeling import train_evaluate_pipeline
+from src.utils import print_evaluation_metrics
+from src.features import GensimVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+def main() -> None:
+
+    train_path = "liar_dataset/train.tsv"
+    test_path = "liar_dataset/test.tsv"
+
+    x_train, y_train = load_and_split_data(train_path)
+    x_test, y_test = load_and_split_data(test_path)
+
+    vectorizer = GensimVectorizer(model_name="glove-twitter-200")
+    classifier = LogisticRegression(max_iter=1000)
+    # classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    model, test_accuracy, test_macro_f1 = train_evaluate_pipeline(x_train, y_train, x_test, y_test, vectorizer, classifier)
+
+    print_evaluation_metrics("H05 - GloVe + Logistic Regression", test_accuracy, test_macro_f1)
+
+if __name__ == '__main__':
+    main()

1ccc7d400fde55dc0cab7c044a821d93c665acfc
Nikolay Boshnakov <nikolay.boshnakov@sap.com>
2026-06-23 19:33:30 +0300

    reordered existing experiment files

--- experiments/13_run_h13_hybrid_bert_updated.py
+++ experiments/13_run_h13_hybrid_bert_updated.py
@@ -1,15 +1,18 @@
 import os
 import time
+
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-import pandas as pd
 from sklearn.metrics import f1_score, precision_score
-from transformers import AutoTokenizer, AutoModel
+from torch.utils.data import DataLoader, Dataset
+from transformers import AutoModel, AutoTokenizer
 
-from fake_news_detector.utils import print_evaluation_metrics, plot_training_history, save_artifacts
-from fake_news_detector.parse_data import MetadataPreprocessor, LiarHybridBertDataset
+from fake_news_detector.modeling import HybridBertFakeNewsNet
+from fake_news_detector.parse_data import LiarHybridBertDataset, MetadataPreprocessor
+from fake_news_detector.utils import (plot_training_history, print_evaluation_metrics,
+                                      save_artifacts)
 
 MODEL_NAME = 'bert-base-uncased'
 MAX_SEQ_LEN = 64
@@ -17,7 +20,8 @@ BATCH_SIZE = 32
 EPOCHS = 4
 LEARNING_RATE = 2e-5
 MIN_CATEGORY_FREQUENCY = 10
-DROPOUT_RATE = 0.4
+DROPOUT_RATE = 0.6
+WEIGHT_DECAY = 0.01
 
 # Mac Hardware Acceleration (Apple Silicon / Metal Performance Shaders)
 if torch.backends.mps.is_available():
@@ -27,24 +31,6 @@ elif torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
-class HybridBertFakeNewsNet(nn.Module):
-    def __init__(self, bert_model_name, meta_dim, num_classes=6, dropout_rate=0.4):
-        super(HybridBertFakeNewsNet, self).__init__()
-
-        self.bert = AutoModel.from_pretrained(bert_model_name)
-        self.meta_fc = nn.Linear(meta_dim, 32)
-        self.meta_relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=dropout_rate)
-        self.fc_out = nn.Linear(self.bert.config.hidden_size + 32, num_classes)
-
-    def forward(self, input_ids, attention_mask, meta_input):
-        bert_outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        text_features = bert_outputs.pooler_output
-        meta_features = self.meta_relu(self.meta_fc(meta_input))
-        combined_features = torch.cat((text_features, meta_features), dim=1)
-        combined_features = self.dropout(combined_features)
-        
-        return self.fc_out(combined_features)
 
 def main():
     print(f"Hardware Acceleration: {device}")
@@ -54,24 +40,45 @@ def main():
     test_path = os.path.join("liar_dataset", "test.tsv")
 
     cols = [
-        "id", "label", "statement", "subjects", "speaker", 
-        "speaker_job", "state_info", "party_affiliation", 
-        "barely_true_counts", "false_counts", "half_true_counts", 
-        "mostly_true_counts", "pants_on_fire_counts", "context"
+        "id", "label", "statement", "subjects", "speaker", "speaker_job",
+        "state_info", "party_affiliation", "barely_true_counts",
+        "false_counts", "half_true_counts", "mostly_true_counts",
+        "pants_on_fire_counts", "context"
     ]
-    df_train = pd.read_csv(train_path, sep='\t', header=None, names=cols, quoting=3).dropna(subset=['statement', 'label'])
-    df_test = pd.read_csv(test_path, sep='\t', header=None, names=cols, quoting=3).dropna(subset=['statement', 'label'])
+    df_train = pd.read_csv(train_path,
+                           sep='\t',
+                           header=None,
+                           names=cols,
+                           quoting=3).dropna(subset=['statement', 'label'])
+    df_test = pd.read_csv(test_path,
+                          sep='\t',
+                          header=None,
+                          names=cols,
+                          quoting=3).dropna(subset=['statement', 'label'])
 
-    num_cols = ["barely_true_counts", "false_counts", "half_true_counts", "mostly_true_counts", "pants_on_fire_counts"]
+    num_cols = [
+        "barely_true_counts", "false_counts", "half_true_counts",
+        "mostly_true_counts", "pants_on_fire_counts"
+    ]
     for col in num_cols:
         df_train[col] = pd.to_numeric(df_train[col], errors='coerce').fillna(0)
         df_test[col] = pd.to_numeric(df_test[col], errors='coerce').fillna(0)
-        
-    cat_cols = ["subjects", "speaker", "speaker_job", "state_info", "party_affiliation", "context"]
+
+    cat_cols = [
+        "subjects", "speaker", "speaker_job", "state_info",
+        "party_affiliation", "context"
+    ]
     df_train[cat_cols] = df_train[cat_cols].fillna("unknown")
     df_test[cat_cols] = df_test[cat_cols].fillna("unknown")
 
-    label_map = {"pants-fire": 0, "false": 1, "barely-true": 2, "half-true": 3, "mostly-true": 4, "true": 5}
+    label_map = {
+        "pants-fire": 0,
+        "false": 1,
+        "barely-true": 2,
+        "half-true": 3,
+        "mostly-true": 4,
+        "true": 5
+    }
     df_train['label_idx'] = df_train['label'].map(label_map)
     df_test['label_idx'] = df_test['label'].map(label_map)
 
@@ -84,17 +91,31 @@ def main():
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    train_dataset = LiarHybridBertDataset(df_train['statement'].values, train_meta_tensor, df_train['label_idx'].values, tokenizer, MAX_SEQ_LEN)
-    test_dataset = LiarHybridBertDataset(df_test['statement'].values, test_meta_tensor, df_test['label_idx'].values, tokenizer, MAX_SEQ_LEN)
+    train_dataset = LiarHybridBertDataset(df_train['statement'].values,
+                                          train_meta_tensor,
+                                          df_train['label_idx'].values,
+                                          tokenizer, MAX_SEQ_LEN)
+    test_dataset = LiarHybridBertDataset(df_test['statement'].values,
+                                         test_meta_tensor,
+                                         df_test['label_idx'].values,
+                                         tokenizer, MAX_SEQ_LEN)
 
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    train_loader = DataLoader(train_dataset,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True)
+    test_loader = DataLoader(test_dataset,
+                             batch_size=BATCH_SIZE,
+                             shuffle=False)
 
-    model = HybridBertFakeNewsNet(bert_model_name=MODEL_NAME, meta_dim=meta_feature_count, dropout_rate=DROPOUT_RATE)
+    model = HybridBertFakeNewsNet(bert_model_name=MODEL_NAME,
+                                  meta_dim=meta_feature_count,
+                                  dropout_rate=DROPOUT_RATE)
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+    optimizer = optim.AdamW(model.parameters(),
+                            lr=LEARNING_RATE,
+                            weight_decay=WEIGHT_DECAY)
 
     history = {'train_loss': [], 'val_loss': [], 'train_f1': [], 'val_f1': []}
     total_start_time = time.time()
@@ -115,7 +136,9 @@ def main():
             meta_features = batch['meta_features'].to(device)
             labels = batch['labels'].to(device)
 
-            logits = model(input_ids=input_ids, attention_mask=attention_mask, meta_input=meta_features)
+            logits = model(input_ids=input_ids,
+                           attention_mask=attention_mask,
+                           meta_input=meta_features)
             loss = criterion(logits, labels)
 
             loss.backward()
@@ -127,7 +150,9 @@ def main():
             train_labels_epoch.extend(labels.cpu().numpy())
 
         avg_train_loss = total_train_loss / len(train_loader)
-        train_f1 = f1_score(train_labels_epoch, train_preds_epoch, average='macro')
+        train_f1 = f1_score(train_labels_epoch,
+                            train_preds_epoch,
+                            average='macro')
 
         model.eval()
         total_val_loss = 0
@@ -141,7 +166,9 @@ def main():
                 meta_features = batch['meta_features'].to(device)
                 labels = batch['labels'].to(device)
 
-                logits = model(input_ids=input_ids, attention_mask=attention_mask, meta_input=meta_features)
+                logits = model(input_ids=input_ids,
+                               attention_mask=attention_mask,
+                               meta_input=meta_features)
                 loss = criterion(logits, labels)
                 total_val_loss += loss.item()
 
@@ -159,22 +186,29 @@ def main():
 
         epoch_time = time.time() - epoch_start_time
         mins, secs = divmod(int(epoch_time), 60)
-        print(f"Epoch [{epoch}/{EPOCHS}] - Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f} | Train F1: {train_f1:.4f} | Val F1: {val_f1:.4f} | Time: {mins}m {secs}s")
+        print(
+            f"Epoch [{epoch}/{EPOCHS}] - Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f} | Train F1: {train_f1:.4f} | Val F1: {val_f1:.4f} | Time: {mins}m {secs}s"
+        )
 
     total_time = time.time() - total_start_time
     total_mins, total_secs = divmod(int(total_time), 60)
 
-    final_macro_prec = precision_score(all_labels, all_preds, average='macro', zero_division=0)
-    
+    final_macro_prec = precision_score(all_labels,
+                                       all_preds,
+                                       average='macro',
+                                       zero_division=0)
+
     print(f"\nTotal Training Time: {total_mins}m {total_secs}s")
-    print_evaluation_metrics("H11 - Hybrid BERT", history['val_f1'][-1], final_macro_prec)
-    plot_training_history(history, "H11_Hybrid_BERT")
-    
+    print_evaluation_metrics("H13 - Hybrid BERT Updated",
+                             history['val_f1'][-1], final_macro_prec)
+    plot_training_history(history, "H13_Hybrid_BERT_Updated")
+
     artifacts_to_save = {
-        "h11_hybrid_bert_weights.pth": model,
-        "h11_metadata_preprocessor.pkl": preprocessor
+        "h13_hybrid_bert_updated_weights.pth": model,
+        "h13_metadata_preprocessor.pkl": preprocessor
     }
     save_artifacts(artifacts_to_save)
+
 
 if __name__ == "__main__":
     main()
